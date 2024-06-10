45e14145d6e4c145acc48c909b447e86f7252c51
Paphada Saengpridikorn <42955313+ccs3312@users.noreply.github.com>
2024-06-10 20:12:02 +0700

    weeee
    weeeeeeee

--- chmltest.py
+++ chmltest.py
@@ -4,13 +4,13 @@ import streamlit as st
 import glob
 from random import shuffle
 import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
 import urllib.request
 from pathlib import Path
 
 from fastai.learner import load_learner
 from fastai.vision.all import PILImage
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 learn_inf = load_learner("fastai_chml.pkl")
 st.sidebar.write('### Enter image to classify')
